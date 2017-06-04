cb24541e08b3e9c25295cbc015dcda4e6c583e3f
RAA <rubenaguirre@outlook.com>
2017-06-04 10:23:01 -0500

    Create archivo_json.js

---
+++ archivo_json.js
@@ -0,0 +1,16 @@
+[
+	{
+		"name":"Meow",
+		"species":"cat",
+		"favFood":"tuna"
+	},
+	{
+		"name":"Ball",
+		"species":"dog",
+		"favFood":"pizza"
+	},
+	{
+		"name":"gl",
+		"species":"dog",
+		"favFood":"tacos"
+	}]
